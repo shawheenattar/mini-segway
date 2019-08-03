53f64e472108325388bc7c410149e86b81263556
Shawheen Attar <shawheenattar@Shawheens-MacBook-Pro.local>
2019-08-02 17:13:44 -0700

    changes

--- sketch_jul20b/sketch_jul20b.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  // put your setup code here, to run once:
-  SerialUSB.begin(9600);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  SerialUSB.println("Hello, World");
-}
